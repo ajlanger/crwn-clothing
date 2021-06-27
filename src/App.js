c3a0c8721e35816763c43e5618883ce1f058e4a9
ajlanger <langeraertarnaud@gmail.com>
2021-06-27 11:34:41 +0200

    updated app with shop page collection preview and shop page collection item.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
+import { Route } from "react-router-dom";
 
-import HomePage from "./pages/homepage/homepage.component";
+import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
 
 function App() {
   return (
     <div>
-      <HomePage />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
     </div>
   );
 }

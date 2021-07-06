311ff0c6529cc9416acb21f43d84fcc2378cc5c3
ajlanger <langeraertarnaud@gmail.com>
2021-07-06 20:24:46 +0200

    Added cart-dropdown component.

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 import "./header.styles.scss";
 
-const Header = ( { currentUser } ) => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="Logo" />
@@ -19,20 +20,25 @@ const Header = ( { currentUser } ) => (
       <Link className="option" to="/contact">
         CONTACT
       </Link>
-      {
-        currentUser ? 
-        <div className='option' onClick={() => auth.signOut()}> SIGN OUT </div>
-        :
-        <Link className='option' to='/signin'> SIGN IN </Link>
-      }
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGN OUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
+      {currentUser ? <CartIcon /> : null}
     </div>
   </div>
 );
 
-const mapStateToProps = (state) => { //state variable will be the top-level root reducer
-  return({
-    currentUser: state.user.currentUser
-  })
-}
+const mapStateToProps = (state) => {
+  //state variable will be the top-level root reducer
+  return {
+    currentUser: state.user.currentUser,
+  };
+};
 
 export default connect(mapStateToProps)(Header);
